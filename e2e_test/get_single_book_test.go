2358b56b78cf747b81b027d86b76fb2ad453ccf2
Shubham Singh <shubhammahar1306@gmail.com>
2024-06-23 08:20:57 +0000

    creating new test get_single_book_test.go
    Signed-off-by: GitHub <noreply@github.com>

--- e2e_test/get_single_book_test.go
+++ e2e_test/get_single_book_test.go
@@ -1,5 +1,3 @@
-// +build e2e
-
 package e2e_test
 
 import (
@@ -13,59 +11,10 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type GetSingleBookSuite struct {
+type GetSingleBookSuite struct{
 	suite.Suite
 }
 
-func TestGetSingleBookSuite(t *testing.T) {
+func TestGetSingleBookSuite(t *testing.T){
 	suite.Run(t, new(GetSingleBookSuite))
-}
-
-var db *sql.DB
-
-func (s *GetSingleBookSuite) SetupTest() {
-	db, _ = sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	db.Exec("TRUNCATE book")
-}
-
-func (s *GetSingleBookSuite) TestGetBookThatDoesNotExist() {
-	c := http.Client{}
-
-	r, _ := c.Get("http://localhost:8080/book/123456789")
-	body, _ := ioutil.ReadAll(r.Body)
-
-	s.Equal(http.StatusNotFound, r.StatusCode)
- 	s.JSONEq(`{"code": "001", "msg": "No book with ISBN 123456789"}`, string(body))
-}
-
-func (s *GetSingleBookSuite) TestGetBookWithInvalidISBNGiven() {
-	c := http.Client{}
-
-	r, _ := c.Get("http://localhost:8080/book/1234C6789")
-	body, _ := ioutil.ReadAll(r.Body)
-
-	s.Equal(http.StatusBadRequest, r.StatusCode)
-	s.JSONEq(`{"code": "003", "msg": "ISBN is invalid"}`, string(body))
-}
-
-func (s *GetSingleBookSuite) TestGetBookThatDoesExist() {
-	s.T().Skip("Pact Demo")
-	db.Exec("INSERT INTO book (isbn, name, image, genre, year_published) VALUES ('987654321', 'Testing All The Things', 'testing.jpg', 'Computing', 2021)")
-
-	c := http.Client{}
-
-	r, _ := c.Get("http://localhost:8080/book/987654321")
-	body, _ := ioutil.ReadAll(r.Body)
-
-	s.Equal(http.StatusOK, r.StatusCode)
-
-	expBody := `{
-	"isbn": "987654321",
-	"title": "Testing All The Things",
-	"image": "testing.jpg",
-	"genre": "Computing",
-	"year_published": 2021
-}`
-
-	s.JSONEq(expBody, string(body))
 }
